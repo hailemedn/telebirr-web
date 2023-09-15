90f25b0108ccfaf6069c40688244c0284033e0e5
hailemedn <hailemedhin.k@gmail.com>
2023-09-15 20:00:14 +0300

    2nd commit

--- src/LogIn.js
+++ src/LogIn.js
@@ -1,20 +1,30 @@
+import { useState } from "react"
+
+
 export default function LogIn() {
+    const [num, setNum] = useState("");
+    const validNum = () => {
+        return(
+          num.length == 9
+        )
+    }
+    
     return (
       <>
         <div className="login-container">
           <div>
             <h1>Welcome to telebirr Web</h1>
-            <p>All-in-One</p>
+            <p>All-in-One </p>
           </div>
-          <div>
+          <div> 
             <h2 className="login-text">Login</h2>
             <form>
               <label className="mobile">Mobile Number</label>
               <div className="input-container">
                   <span className="prefix">+251</span>
-                  <input  type="number" placeholder="Enter Phone Number"></input>
+                  <input  type="number" placeholder="Enter Phone Number"  value={num} onChange={e => setNum(e.target.value)}></input>
               </div>
-              <button className="btn-primary" type="submit">Next</button>
+              <button className="btn-primary" type="submit" disabled={!validNum()}>Next</button>
               <p>Don't have an Account? <a href="#">Create New Account</a></p>
               </form>
           </div>
