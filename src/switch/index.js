1888716faa2aae1a4d0f53a0167533df9bf2f38d
hailemedn <hailemedhin.k@gmail.com>
2023-09-19 23:55:17 +0300

    styled the checkbox input for the theme switcher

--- src/switch/index.js
+++ src/switch/index.js
@@ -1,9 +1,17 @@
 import { useTheme } from "../ThemeContext";
-
+import './Styles.css'
 const Switch = () => {
     const {theme, toggleTheme} = useTheme();
     return (
-        <input type="checkbox" checked={theme==="dark"} onChange={toggleTheme}></input>
+        <label className="switch">
+            <input 
+                type="checkbox" 
+                checked={theme==="dark"} 
+                onChange={toggleTheme}>
+            
+            </input>
+            <span className="slider round"/>
+        </label>
     )
 }
 
